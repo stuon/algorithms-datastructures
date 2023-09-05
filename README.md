f360bec0bfe3a9d1773b41dd84ca9ddafeb8df8d
San Tuon <san.tuon@gmail.com>
2023-09-06 07:01:23 +1000

    added max char problem

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [Reverse String in Javascript](reverse_string.md)
 - [Palindrome in Javascript](palindrome.md)
 - [Reverse Integer in Javascript](reverse_integer.md)
+- [Max Char in Javascript](max_char.md)
---
+++ code_examples/max_char.js
@@ -0,0 +1,63 @@
+
+// Max Char Problem is defined as follows:
+// Given a string, return the character that occurs the most frequently.
+
+// Test Cases
+console.log(maxChar("hello")); // Expected output: 'l' (occurs 2 times)
+console.log(maxChar("programming")); // Expected output: 'g' (occurs 2 times)
+console.log(maxChar("aabbcc")); // Expected output: 'a' (occurs 2 times)
+console.log(maxChar("")); // Expected output: '' (empty string)
+console.log(maxChar("aaaaa")); // Expected output: 'a' (occurs 5 times)
+console.log(maxChar("12345")); // Expected output: '1' (occurs 1 time)
+console.log(maxChar("  a b c d e f ")); // Expected output: ' ' (space occurs 7 times)
+console.log(maxChar("!@#$%^&*()")); // Expected output: '!' (occurs 1 time)
+
+
+// Approach 1: Brute Force
+
+function findMaxChar(str) {
+  const charCount = {};
+  
+  for (const char of str) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+  
+  let maxChar = '';
+  let maxCount = 0;
+  
+  for (const char in charCount) {
+    if (charCount[char] > maxCount) {
+      maxChar = char;
+      maxCount = charCount[char];
+    }
+  }
+  
+  return maxChar;
+}
+
+
+// Approach 2: Using a Map
+
+function findMaxChar(str) {
+  const charCount = new Map();
+  
+  for (const char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+  
+  let maxChar = '';
+  let maxCount = 0;
+  
+  charCount.forEach((count, char) => {
+    if (count > maxCount) {
+      maxChar = char;
+      maxCount = count;
+    }
+  });
+  
+  return maxChar;
+}
