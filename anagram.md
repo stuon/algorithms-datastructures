db4a603d3112580349d0bcf97fb7b7d16d3272c3
San Tuon <san.tuon@gmail.com>
2023-09-10 21:22:13 +1000

    fix test cases in anagram

--- anagram.md
+++ anagram.md
@@ -4,26 +4,29 @@ Anagrams are words or phrases formed by rearranging the letters of another word 
 
 ## Test Cases
 ```javascript
-// Test Case 1: Basic anagrams
-console.log("Test Case 1:", areAnagrams("listen", "silent")); // Expected output: true
+// Test Case 1: Basic anagram
+console.log(areAnagrams("listen", "silent"), "Expected: true");
 
-// Test Case 2: Anagrams with different letter cases
-console.log("Test Case 2:", areAnagrams("Hello", "oLleh")); // Expected output: true
+// Test Case 2: Anagram with different letter cases
+console.log(areAnagrams("Tea", "Eat"), "Expected: true");
 
-// Test Case 3: Words with different lengths
-console.log("Test Case 3:", areAnagrams("apple", "pale")); // Expected output: false
+// Test Case 3: Non-anagram with different lengths
+console.log(areAnagrams("hello", "world"), "Expected: false");
 
-// Test Case 4: Anagrams with special characters
-console.log("Test Case 4:", areAnagrams("dormitory", "dirty room##")); // Expected output: true
+// Test Case 4: Non-anagram with the same letters but different counts
+console.log(areAnagrams("race", "car"), "Expected: false");
 
-// Test Case 5: Non-anagrams with the same characters
-console.log("Test Case 5:", areAnagrams("hello", "world")); // Expected output: false
+// Test Case 5: Anagram with repeated letters
+console.log(areAnagrams("debit card", "bad credit"), "Expected: true");
 
-// Test Case 6: Empty strings
-console.log("Test Case 6:", areAnagrams("", "")); // Expected output: true
+// Test Case 6: Anagram with numbers
+console.log(areAnagrams("12345", "54321"), "Expected: true");
 
-// Test Case 7: Anagrams with spaces
-console.log("Test Case 7:", areAnagrams("a gentleman", "elegant man")); // Expected output: true
+// Test Case 7: Empty strings are considered anagrams
+console.log(areAnagrams("", ""), "Expected: true");
+
+// Test Case 8: Anagrams with special characters
+console.log(areAnagrams("dormitory", "dirty room##"), "Expected: true");
 ```
 
 ## Method 1: Sorting and Comparing
@@ -32,10 +35,10 @@ One of the simplest ways to check if two words are anagrams is to sort the chara
 
 ```javascript
 function areAnagrams(word1, word2) {
-  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, '');
-  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, '');
+  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, "");
+  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, "");
 
-  return word1.split('').sort().join('') === word2.split('').sort().join('');
+  return word1.split("").sort().join("") === word2.split("").sort().join("");
 }
 ```
 
@@ -46,9 +49,9 @@ function areAnagrams(word1, word2) {
 Another approach is to create a frequency map of characters in each word and compare the maps. If the frequency of each character in both maps is the same, the words are anagrams.
 
 ```javascript
-function areAnagrams(word1, word2) {
-  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, '');
-  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, '');
+function areAnagramsMap(word1, word2) {
+  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, "");
+  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, "");
 
   if (word1.length !== word2.length) return false;
 
@@ -78,9 +81,9 @@ function areAnagrams(word1, word2) {
 We can use arrays to count the occurrences of each character in both words and compare the arrays.
 
 ```javascript
-function areAnagrams(word1, word2) {
-  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, '');
-  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, '');
+function areAnagramsArray(word1, word2) {
+  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, "");
+  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, "");
 
   if (word1.length !== word2.length) return false;
 
@@ -88,16 +91,16 @@ function areAnagrams(word1, word2) {
   const charCount2 = Array(26).fill(0);
 
   for (let i = 0; i < word1.length; i++) {
-    const index = word1.charCodeAt(i) - 'a'.charCodeAt(0);
+    const index = word1.charCodeAt(i) - "a".charCodeAt(0);
     charCount1[index]++;
   }
 
   for (let i = 0; i < word2.length; i++) {
-    const index = word2.charCodeAt(i) - 'a'.charCodeAt(0);
+    const index = word2.charCodeAt(i) - "a".charCodeAt(0);
     charCount2[index]++;
   }
 
-  return charCount1.join('') === charCount2.join('');
+  return charCount1.join("") === charCount2.join("");
 }
 ```
 
--- code_examples/anagram.js
+++ code_examples/anagram.js
@@ -3,84 +3,83 @@
 // using all the original letters exactly once. 
 // Consider only alphanumeric characters and ignore letter cases. 
 
+// Test Case 1: Basic anagram
+console.log(areAnagrams("listen", "silent"), "Expected: true");
 
-// Test Case 1: Basic anagrams
-console.log("Test Case 1:", areAnagrams("listen", "silent")); // Expected output: true
+// Test Case 2: Anagram with different letter cases
+console.log(areAnagrams("Tea", "Eat"), "Expected: true");
 
-// Test Case 2: Anagrams with different letter cases
-console.log("Test Case 2:", areAnagrams("Hello", "oLleh")); // Expected output: true
+// Test Case 3: Non-anagram with different lengths
+console.log(areAnagrams("hello", "world"), "Expected: false");
 
-// Test Case 3: Words with different lengths
-console.log("Test Case 3:", areAnagrams("apple", "pale")); // Expected output: false
+// Test Case 4: Non-anagram with the same letters but different counts
+console.log(areAnagrams("race", "car"), "Expected: false");
 
-// Test Case 4: Anagrams with special characters
-console.log("Test Case 4:", areAnagrams("dormitory", "dirty room##")); // Expected output: true
+// Test Case 5: Anagram with repeated letters
+console.log(areAnagrams("debit card", "bad credit"), "Expected: true");
 
-// Test Case 5: Non-anagrams with the same characters
-console.log("Test Case 5:", areAnagrams("hello", "world")); // Expected output: false
+// Test Case 6: Anagram with numbers
+console.log(areAnagrams("12345", "54321"), "Expected: true");
 
-// Test Case 6: Empty strings
-console.log("Test Case 6:", areAnagrams("", "")); // Expected output: true
+// Test Case 7: Empty strings are considered anagrams
+console.log(areAnagrams("", ""), "Expected: true");
 
-// Test Case 7: Anagrams with spaces
-console.log("Test Case 7:", areAnagrams("a gentleman", "elegant man")); // Expected output: true
-
+// Test Case 8: Anagrams with special characters
+console.log(areAnagrams("dormitory", "dirty room##"), "Expected: true");
 
 // Method 1: Sorting and Comparing
-
 function areAnagramsSort(word1, word2) {
-    word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, '');
-    word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, '');
-  
-    return word1.split('').sort().join('') === word2.split('').sort().join('');
-}
+  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, "");
+  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  return word1.split("").sort().join("") === word2.split("").sort().join("");
+}
 
 //  Method 2: Character Frequency Map
 function areAnagramsMap(word1, word2) {
-    word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, '');
-    word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, '');
-  
-    if (word1.length !== word2.length) return false;
-  
-    const charMap1 = {};
-    const charMap2 = {};
-  
-    for (let char of word1) {
-      charMap1[char] = (charMap1[char] || 0) + 1;
-    }
-  
-    for (let char of word2) {
-      charMap2[char] = (charMap2[char] || 0) + 1;
-    }
-  
-    for (let char in charMap1) {
-      if (charMap1[char] !== charMap2[char]) return false;
-    }
-  
-    return true;
+  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, "");
+  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  if (word1.length !== word2.length) return false;
+
+  const charMap1 = {};
+  const charMap2 = {};
+
+  for (let char of word1) {
+    charMap1[char] = (charMap1[char] || 0) + 1;
+  }
+
+  for (let char of word2) {
+    charMap2[char] = (charMap2[char] || 0) + 1;
+  }
+
+  for (let char in charMap1) {
+    if (charMap1[char] !== charMap2[char]) return false;
+  }
+
+  return true;
 }
 
 // Method 3: Array-based Approach
 
 function areAnagramsArray(word1, word2) {
-    word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, '');
-    word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, '');
-  
-    if (word1.length !== word2.length) return false;
-  
-    const charCount1 = Array(26).fill(0);
-    const charCount2 = Array(26).fill(0);
-  
-    for (let i = 0; i < word1.length; i++) {
-      const index = word1.charCodeAt(i) - 'a'.charCodeAt(0);
-      charCount1[index]++;
-    }
-  
-    for (let i = 0; i < word2.length; i++) {
-      const index = word2.charCodeAt(i) - 'a'.charCodeAt(0);
-      charCount2[index]++;
-    }
-  
-    return charCount1.join('') === charCount2.join('');
+  word1 = word1.toLowerCase().replace(/[^a-z0-9]/g, "");
+  word2 = word2.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  if (word1.length !== word2.length) return false;
+
+  const charCount1 = Array(26).fill(0);
+  const charCount2 = Array(26).fill(0);
+
+  for (let i = 0; i < word1.length; i++) {
+    const index = word1.charCodeAt(i) - "a".charCodeAt(0);
+    charCount1[index]++;
+  }
+
+  for (let i = 0; i < word2.length; i++) {
+    const index = word2.charCodeAt(i) - "a".charCodeAt(0);
+    charCount2[index]++;
+  }
+
+  return charCount1.join("") === charCount2.join("");
 }
